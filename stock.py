80fdc0d9988922b8d1cf165a3b7ffdda68a0970a
Aniket Gawade <aniket.mailbot@gmail.com>
2018-01-29 15:49:39 -0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -2,17 +2,11 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
-import datetime
-#you can enable this to get timezone tzinfo object
-#import pytz
-page = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={NASDAQ ID}&interval=1min&apikey={yourapikey}')
+page = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=1min&apikey={API KEY}')
 if page.status_code == 200:
     data = json.loads(page.content)
-    curr_date = datetime.datetime.now() #To chagne timezone(pytz.timezone("America/New_York"))
-    #Changing to EST and delayed by 1 min 
-    delta_date = curr_date - datetime.timedelta(minutes=1)
-    delta_date = delta_date + datetime.timedelta(hours=3)
-    recs = data["Time Series (1min)"][delta_date.strftime("%Y-%m-%d %H:%M:00")]["2. high"]
-    print "(%s)" %(recs)
+    for rec in sorted(data["Time Series (1min)"],reverse=True):
+        print "(%s)" %(data["Time Series (1min)"][rec]["2. high"])
+        break
 else:
     print "(ERR)"
